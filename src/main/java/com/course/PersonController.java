6994b795cb9d04888a1a03565e6cdcdf150d0cbb
Thiago Torres <thiagomev@gmail.com>
2024-03-04 17:51:14 -0300

    Removed imports disable

--- src/main/java/com/course/PersonController.java
+++ src/main/java/com/course/PersonController.java
@@ -2,7 +2,6 @@ package com.course;
 
 import org.springframework.web.bind.annotation.RestController;
 
-import com.course.exceptions.UnsupportedMathOperationException;
 import com.course.model.Person;
 import com.course.services.PersonServices;
 
@@ -17,8 +16,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class PersonController {
     @Autowired
     private PersonServices service;
-    // private static final String template = "Hello, %s!";
-    // private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public Person findById(@PathVariable(value = "id") String id) throws Exception {
